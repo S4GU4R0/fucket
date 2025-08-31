c30a77629b3032e5c529736805c10d1d9b60e7fa
S4GU4R0 <162850571+S4GU4R0@users.noreply.github.com>
2025-08-31 23:44:20 +0000

    i hate eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-unused-vars': 0,
   }
 }
